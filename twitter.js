038f563c327fa9dd9faa50fe7bb5503ccb71e32b
ranitan <ranitanagarajan@gmail.com>
2023-05-23 00:46:12 +0530

    crud

---
+++ twitter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // Fetch cake data from the JSON file
+    $.getJSON('twitter.json', function(data) {
+      var specialsContainer = $('#specials-container');
+     
+      
+      $.each(data.twitter, function(index, item) {
+        var twitter = $('<div class="item"></div>');
+        twitter.append('<img src="'+ item.image + '" alt="' + item.name + '" id="img"> ');
+        twitter.append('<h3 id="ca1">' + item.name + '</h3>');
+        twitter.append('<a href="#sec"><button type="button" class="btn btn-success" id="atc" >Tweet</button></a>')
+        specialsContainer.append(twitter);
+      });
+   
+    });
+  });
+
+ 
