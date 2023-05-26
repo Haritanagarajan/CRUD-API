b461f4d9eb359ede7070b0b040e5d2ba6a1453e1
Haritanagarajan <30harita2002@gmail.com>
2023-05-26 13:02:59 +0530

    crud

--- twitter.js
+++ twitter.js
@@ -1,4 +1,5 @@
 
+//user create for get
 
 function validateapiform() {
   console.log("userlogin");
@@ -13,17 +14,82 @@ function validateapiform() {
       for (const twitt of objects) {
         if (username == twitt["username"] && (password == twitt["password"])) {
           console.log("success");
-          window.location.replace("twitterhomepage.html");
+          loggeduserEdit(twitt['id']);
           return false;
         }
       }
       window.alert("Not a valid Twitter User");
     }
+
   }
 
 }
 
+//logged user with edit
 
+function loggeduserEdit(id) {
+  console.log("login user edit");
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("PUT", `http://localhost:3000/userlogin/${id}`);
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(
+    JSON.stringify({
+      username: username,
+      password: password,
+      login: 1,
+
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      Swal.fire(objects["message"]);
+      window.location.replace("twitterhomepage.html");
+    }
+  };
+}
+
+//logout user with get and edit
+
+function logoutuserEdit() {
+  const xhttpp = new XMLHttpRequest();
+  xhttpp.open("GET", "http://localhost:3000/userlogin");
+  xhttpp.send();
+  xhttpp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      for (const twitt of objects) {
+        if (twitt['login']==1) {
+          const xhttpObj = new XMLHttpRequest();
+          xhttpObj.open("PUT", `http://localhost:3000/userlogin/${twitt['id']}`);
+          xhttpObj.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          xhttpObj.send(
+            JSON.stringify({
+              username: twitt['username'],
+              password: twitt['password'],
+              login: 0,
+    
+            })
+          );
+          xhttpObj.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              const objects = JSON.parse(this.responseText);
+              Swal.fire(objects["message"]);
+              window.location.replace("twitterlogin.html");
+            }
+          };
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+
+//create user edit
 
 function createtwitteruser(event) {
   event.preventDefault();
@@ -50,25 +116,28 @@ function createtwitteruser(event) {
 // json-server --watch <jsonfilename.json>
 
 
-function adminreadvalue(event){
+
+//admin login with get
+
+function adminreadvalue(event) {
   event.preventDefault();
   console.log("adminlogin");
-  const adminusername= document.getElementById("adminusername").value;
+  const adminusername = document.getElementById("adminusername").value;
   const adminpassword = document.getElementById("adminpassword").value;
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://localhost:3000/adminlogin");
   xhttp.send();
-  console.log(adminpassword,adminusername)
+  console.log(adminpassword, adminusername)
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const objects = JSON.parse(this.responseText);
       for (const twitt of objects) {
-        if (adminusername== twitt["aname"] && (adminpassword == twitt["apassword"])) {
+        if (adminusername == twitt["aname"] && (adminpassword == twitt["apassword"])) {
           window.location.replace("index.html");
           return false;
         }
       }
-     window.alert("Not a valid Twitter Admin");
+      window.alert("Not a valid Twitter Admin");
     }
   }
 
