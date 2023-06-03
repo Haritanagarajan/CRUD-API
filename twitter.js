40a18cc47c4f4b0ef6dfc08d748e5efddab679c6
Haritanagarajan <30harita2002@gmail.com>
2023-06-03 16:50:10 +0530

    CRUD

--- twitter.js
+++ twitter.js
@@ -148,64 +148,64 @@ function adminreadvalue(event) {
 //tweet the message
 
 
-function createtweetmsg() {
-  console.log("userlogin");
-  const message = document.getElementById("message").value;
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:3000/userlogin");
-  xhttp.send();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const objects = JSON.parse(this.responseText);
-      for (const twitt of objects) {
-        if ((message == twitt["tweetmsg"])) {
-          console.log("success");
-          loggeduserEdit(twitt['id']);
-          return false;
-        }
-      }
-    }
-
-  }
-
-}
 
 
 function showtweetmsgbox(event) {
   event.preventDefault();
 
   Swal.fire({
-      title: "Create user",
-      customClass: {
-          popup: 'red-popup',
-      },
-      html:
-          '<form id="myform"  class="is-validated">' +
-          '<div class="swal2-row">' +
-          '<input id="id" type="hidden" class="swal2-input">' +
-          "</div>" +
+    title: "Create user",
+    customClass: {
+      popup: 'red-popup',
+    },
+    html:
+      '<form id="myform"  class="is-validated">' +
+      '<div class="swal2-row">' +
+      '<input id="id" type="hidden" class="swal2-input">' +
+      "</div>" +
 
 
-          '<div class="swal2-row">' +
-          '<label for="username">UserName</label>' +
-          '<input id="username" name="username" class="swal2-input" placeholder="UserName" pattern="[a-zA-Z]+" required>' +
-          '</div>' +
+      '<div class="swal2-row">' +
+      '<label for="username">UserName</label>' +
+      '<input id="username" name="username" class="swal2-input" placeholder="UserName" pattern="[a-zA-Z]+" required>' +
+      '</div>' +
 
-          '<div class="swal2-row">' +
-          '<label for="message">Message</label>' +
-          '<input id="message" class="swal2-input" type="textarea" placeholder="message" required>' +
-          "</div>" ,
+      '<div class="swal2-row">' +
+      '<label for="message">Message</label>' +
+      '<input id="message" class="swal2-input" type="textarea" placeholder="message" required>' +
+      "</div>",
 
-      showCancelButton: true,
+    showCancelButton: true,
 
-      preConfirm: async () => {
+    preConfirm: async () => {
 
-          
-        createtweetmsg();    
 
-      },
+      twittermessage(event)
+
+    },
   });
 }
 
+function twittermessage(event) {
+  event.preventDefault();
+  console.log("message");
+  const message = document.getElementById("message").value;
+  const username = document.getElementById("username").value;
+console.log(message);
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://localhost:3000/Twittermessage");
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const objects = JSON.parse(this.responseText);
+      for (const twitt of objects) {
+        if ((message == twitt["twittmsg"]) && (username==twitt["username"])) {
+          return false;
+        }
+      }
+      window.alert("Not a valid Twitter Admin");
+    }
+  }
 
+}
 
