e89e465d37187a23b22f9b88a57593c4c275fb31
Haritanagarajan <30harita2002@gmail.com>
2023-06-05 16:50:22 +0530

    twitter

--- twitter.js
+++ twitter.js
@@ -111,15 +111,6 @@ function createtwitteruser(event) {
 
 }
 
-// Use the following command to install JSON Server
-// npm install -g json-server
-
-// Use this command to get access to JSON data
-// json-server --watch <jsonfilename.json>
-
-
-
-//admin login with get
 
 function adminreadvalue(event) {
   event.preventDefault();
@@ -145,67 +136,6 @@ function adminreadvalue(event) {
 
 }
 
-//tweet the message
+console.log("hi")
 
-
-
-
-function showtweetmsgbox(event) {
-  event.preventDefault();
-
-  Swal.fire({
-    title: "Create user",
-    customClass: {
-      popup: 'red-popup',
-    },
-    html:
-      '<form id="myform"  class="is-validated">' +
-      '<div class="swal2-row">' +
-      '<input id="id" type="hidden" class="swal2-input">' +
-      "</div>" +
-
-
-      '<div class="swal2-row">' +
-      '<label for="username">UserName</label>' +
-      '<input id="username" name="username" class="swal2-input" placeholder="UserName" pattern="[a-zA-Z]+" required>' +
-      '</div>' +
-
-      '<div class="swal2-row">' +
-      '<label for="message">Message</label>' +
-      '<input id="message" class="swal2-input" type="textarea" placeholder="message" required>' +
-      "</div>",
-
-    showCancelButton: true,
-
-    preConfirm: async () => {
-
-
-      twittermessage(event)
-
-    },
-  });
-}
-
-function twittermessage(event) {
-  event.preventDefault();
-  console.log("message");
-  const message = document.getElementById("message").value;
-  const username = document.getElementById("username").value;
-console.log(message);
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:3000/Twittermessage");
-  xhttp.send();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const objects = JSON.parse(this.responseText);
-      for (const twitt of objects) {
-        if ((message == twitt["twittmsg"]) && (username==twitt["username"])) {
-          return false;
-        }
-      }
-      window.alert("Not a valid Twitter Admin");
-    }
-  }
-
-}
 
